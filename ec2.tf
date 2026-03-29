00b4defef941cbc96ab653a5192287a4b9667bac
Your Name <you@example.com>
2026-03-29 23:32:40 +0530

    committing

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "instance1" {
-  region = "ap-east-1"
-  ami = "ami16528898723ddf"
-  security_groups = "tyutv36790gjdv"
+  region = "ap-south-1"
+  ami = "ami-05d2d839d4f73aafb"
 }
